93f6e6a3a9c207333f7c1851073fdf4c7fe41bec
dsaraj <73612307+dsaraj@users.noreply.github.com>
2022-04-24 19:47:03 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+"""import Flask and SQlite packages"""
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect 
 from werkzeug.exceptions import abort
 
+"""
+function opens connection to database.db file. Sets row_factory attribute to sqlite3.Row to gain access to name based access to columns
+Database connection will return rows that behave like regular Python dictionaries.
+Conn.close() closes database connection and returns results. 
+"""
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
-    return conn
+    return conn #returns conn connection object to access database
 
+"""
+get_post(post_id) determines what blog plost to return
+use get_db_connection() function to open db connection and execute SQL query to 
+get blost post associated with give post_id value.
+fetchone() method used to get result and store it in post variable and close connection.
+If post variable has None value the abort() function will respond with 404 error code.
+"""
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
@@ -19,6 +32,11 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vgy7bhu8VGY&BHU*'
 
+"""
+Open database connection using get_db_connection() function.
+posts function executes SQL query to select all entries from 'posts' table.
+fetchall() method fetches all rows of query result
+"""
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -26,6 +44,12 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+"""
+new function adds variable <int:post_id> to specify part after (/) is a positive integer
+Flask recognizes this and passes its value to post_id of post() view function
+get_post() function gets blog post associated with specified ID and stores result in post variable,
+which is passed to post.html template.
+"""
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
